13ea79724f22c1030f51d7c04fbf4d2b3a326543
Your Name <you@example.com>
2024-03-13 21:40:21 -0300

    coisoapi

---
+++ coiso/src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "ec85a325c4msh6b105d03c8b1ecap1fce1bjsn5cd417ad7282",
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "0eb9521687ae83f234f40bf78c075c77"; 
+  
